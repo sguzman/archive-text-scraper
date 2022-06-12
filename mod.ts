af7874f2a2c6fe8390805fb19f137cff902306a9
Salvador Guzman <guzmansalv@gmail.com>
2022-06-11 18:23:34 -0700

    looking good so far

--- mod.ts
+++ mod.ts
@@ -1,146 +1,121 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 import { IUrlParse, urlParse } from "https://deno.land/x/url_parse/mod.ts";
 
-// ----------------------------------------------------------------------------
-
 import * as cheerio from "https://esm.sh/cheerio";
 
-// ----------------------------------------------------------------------------
-
-log.info(`Received library: ${Deno.args[0]}`);
-
-function library(): IUrlParse {
-  const url = urlParse({
-    protocol: "https",
-    hostname: "archive.org",
-    pathname: `/details/${Deno.args[0]}`,
-  });
-
-  return url;
+interface YearCount {
+  val: number;
+  n: number;
+  txt: string | undefined;
 }
 
-function link(text: string): IUrlParse {
-  const url = urlParse({
-    protocol: "https",
-    hostname: "archive.org",
-    pathname: `/details/${text}`,
-  });
+const libId = Deno.args[0];
+const pageSize = 75;
 
-  return url;
+const util = {
+  encode: (str: string) => {
+    return encodeURIComponent(str).replace(/%20/g, "+");
+  },
+  url: {
+    count: urlParse(
+      `https://archive.org/details/${libId}&headless=1&facets_xhr=facets&morf=year&output=json`,
+    ),
+    year: (year: number): IUrlParse => {
+      return urlParse(
+        `https://archive.org/details/${libId}&and[]=date%3A${year}`,
+      );
+    },
+    page: (year: number, page: number) => {
+      return urlParse(
+        `https://archive.org/details/${libId}?and%5B%5D=mediatype%3A%22texts%22&and%5B%5D=year%3A%22${year}%22&sort=-week&page=${page}&scroll=1
+        `,
+      );
+    },
+    details: (itemId: string): IUrlParse => {
+      return urlParse(`https://archive.org/details/${itemId}/`);
+    },
+  },
+};
+
+// Read file
+export async function readFile(file: string): Promise<string> {
+  const data = await Deno.readFile(file);
+  return new TextDecoder("utf-8").decode(data);
 }
 
-function page(n: number): IUrlParse {
-  const url = urlParse({
-    protocol: "https",
-    hostname: "archive.org",
-    pathname: `/details/${Deno.args[0]}`,
-    query: [{
-      key: "and[]",
-      value: `mediatype:"texts"`,
-    }, {
-      key: "sort",
-      value: "-week",
-    }, {
-      key: "page",
-      value: `${n}`,
-    }, {
-      key: "scroll",
-      value: "1",
-    }],
-  });
-
-  log.info(`Built ${url.toString()}`);
-  return url;
+// Write file
+export async function writeFile(file: string, data: string): Promise<void> {
+  await Deno.writeFile(file, new TextEncoder().encode(data));
 }
 
-async function _exists(filename: string): Promise<boolean> {
+// Does file exists
+export async function fileExists(file: string): Promise<boolean> {
   try {
-    await Deno.stat(filename);
-    // successful, file or directory must exist
+    await Deno.stat(file);
     return true;
-  } catch (_error) {
-    // error, file or directory must not exist
+  } catch (_) {
     return false;
   }
 }
 
-async function _get(url: IUrlParse): Promise<string> {
-  const response = await fetch(url.toString());
-  const text = await response.text();
+// If file exists, return it. otherwise create file from function
+export async function cacheOrFetch(
+  url: IUrlParse,
+): Promise<string> {
+  const filePath = `./data/${encodeURIComponent(url.toString())}.html`;
+
+  if (await fileExists(filePath)) {
+    return readFile(filePath);
+  } else {
+    const resp = await fetch(url.toString());
+    const data = await resp.text();
+    await writeFile(filePath, data);
+
+    return data;
+  }
+}
+
+// Fetch url and return text
+export async function fetchUrl(url: IUrlParse): Promise<string> {
+  const text = await cacheOrFetch(url);
   return text;
 }
 
-async function _save(filename: string, text: string): Promise<void> {
-  const file = await Deno.open(filename, {
-    create: true,
-    write: true,
-    truncate: true,
-  });
+// Get counts from url
+export async function getCounts(url: string): Promise<YearCount[]> {
+  const res = await fetch(url);
+  const json = await res.json();
+  const counts: YearCount[] = json.options;
 
-  await Deno.writeAll(file, new TextEncoder().encode(text));
-  await file.close();
+  return counts;
 }
 
-async function readFile(filename: string): Promise<string> {
-  const file = await Deno.open(filename);
-  const text = await Deno.readAll(file);
-  await file.close();
-  return text.toString();
-}
+log.info("Starting...");
 
-async function cache(url: IUrlParse): Promise<string> {
-  const filePath = url.pathname;
+for (const obj of await getCounts(util.url.count.toString())) {
+  const year = obj.val;
+  const count = obj.n;
+  const pages = Math.ceil(count / pageSize);
 
-  if (filePath && typeof filePath === "string") {
-    const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
-    const fullFilePath = `./data/details/${fileName}.html`;
+  log.info(`Year ${year} | count = ${count} | pages = ${pages}`);
 
-    const exists = await _exists(fullFilePath);
+  for (let i = 1; i <= pages; i++) {
+    const url = util.url.page(year, i);
+    const text = await fetchUrl(url);
+    const $ = cheerio.load(text);
 
-    if (exists) {
-      return await readFile(fullFilePath);
-    } else {
-      log.info(`Fetching ${fileName}`);
-      const text = await _get(url);
-      await _save(fullFilePath, text);
-      return text;
-    }
-  }
+    const items = $("div.item-ttl > a[href]");
+    log.info(`Year ${year} | page ${i} | items = ${items.length}`);
 
-  return "";
-}
+    for (const item of items.toArray()) {
+      const loadElement = $(item);
+      const itemhref = loadElement.attr("href") as string;
+      log.info(`Year ${year} | page ${i} | item = ${itemhref}`);
+      const itemId = itemhref.split("/")[-1];
 
-const start: Response = await fetch(library().toString());
-const html: string = await start.text();
-
-const $ = cheerio.load(html);
-const resultCount: number = parseInt(
-  $("div.results_count").text().trim().split("\n").map((s) => s.trim())[0]
-    .replace(",", ""),
-  10,
-);
-
-log.info(`Found ${resultCount} results`);
-
-const pages: number = Math.ceil(resultCount / 75);
-log.info(`Found ${pages} pages`);
-
-for (let i = 1; i < pages; ++i) {
-  log.info(`Fetching page ${i}`);
-
-  const body: Response = await fetch(page(i).toString());
-  const text: string = await body.text();
-  const doc = cheerio.load(text);
-
-  const items = doc("[data-id]");
-  const itemArray = items.toArray();
-
-  for (const item of itemArray) {
-    const u = $(item).attr("data-id");
-    if (u) {
-      const url = link(u);
-      const text = await cache(url);
-      log.info(`Cached ${url.toString()}`);
+      const itemUrl = util.url.details(itemId);
+      const itemText = await fetchUrl(itemUrl);
+      const $item = cheerio.load(itemText);
     }
   }
 }
